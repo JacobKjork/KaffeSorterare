36bd7db7c77d6d876a431dd8dcefff1d9828d2a0
Jacob Kjörk <jacob.kjork@gmail.com>
2023-10-18 08:47:38 +0200

    fixade ett formulär och sätt att spara

--- KaffeSorterare/app.js
+++ KaffeSorterare/app.js
@@ -1,9 +1,13 @@
+const fs = require('fs');
+const xml2js = require('xml2js');
+const bodyParser = require('body-parser');
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+const filePath = path.join(__dirname, '/public/fileLoader.xml');
 
  const app = express();
 
@@ -15,6 +19,7 @@ const path = require('path');
    headers: myHeaders,
    redirect: 'follow'
  };
+
  var obj;
  fetch("https://www.vattenfall.se/api/price/spot/pricearea/2023-10-16/2023-10-16/SN3/", requestOptions)
    .then(response => response.text())
@@ -23,9 +28,10 @@ const path = require('path');
       console.log(result)})
    .catch(error => console.log('error', error));
 
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads   
 app.use(morgan('combined'));
-
+app
 //vi kollar alltid om det finns en statisk websida i public innan
 //vi ser om det finns en vy eller ett statiskt meddelande.
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -51,10 +57,79 @@ app.set('view engine', 'ejs');
 
 
  //här skickas bara ett statiskt meddelande
+
+ app.post('/spara', (req, res)=>{
+
+   var people = [];
+   // Läs innehållet från XML-filen
+   fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        console.error('Fel vid läsning av filen:', err);
+        return;
+      }
+    
+      // Konvertera XML till JSON med xml2js
+      xml2js.parseString(data, (parseErr, result) => {
+        if (parseErr) {
+          console.error('Fel vid konvertering från XML till JSON:', parseErr);
+          return;
+        }
+    
+        // Resultatet är nu ett JSON-objekt
+        console.log('Innehållet i XML-filen som JSON:', result);
+    
+        // Du kan komma åt personerna som ett JavaScript-objekt
+         people = result.people.person;
+    
+        // Gör något med personerna här...
+
+// Lägg till en ny person
+const newPerson = { firstname: req.body.firstName, lastname: req.body.lastName, email: req.body.email, birthday: req.body.birthday };
+people.push(newPerson);
+
+// Skapa XML-strängen från personobjekten
+const xmlString = `<people>
+${people.map(person => `
+  <person>
+    <firstname>${person.firstname}</firstname>
+    <lastname>${person.lastname}</lastname>
+    <email>${person.email}</email>
+    <birthday>${person.birthday}</birthday>
+  </person>`).join('\n')}
+</people>`;
+
+
+// Skriv XML-strängen till filen
+fs.writeFile(filePath, xmlString, (err) => {
+   if (err) {
+     console.error('Fel vid skrivning till fil:', err);
+   } else {
+     console.log('XML-filen har skrivits till disk:', filePath);
+   }
+ });
+
+ res.redirect( '/test.html');
+      });
+    });
+
+
+
+
+ });
+
+
+
+
  app.get('/hej',(req, res)=>{
 
-    res.send('hej world');
- });
+   res.send('hej world');
+});
+
+app.get('/svara',(req, res)=>{
+
+  res.send('hej svar');
+});
+
 
  app.listen(3000, ()=>{
     debug('lyssnar på port 3000' );
